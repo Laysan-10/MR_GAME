dcadccbdcbdb9d1d2d9d16a4a9d9416adce4286f
Laysan-10 <marichkahh@gmail.com>
2024-11-15 03:53:19 +0300

    activate_panel

--- Assets/Skripts/Spawn_Object.cs
+++ Assets/Skripts/Spawn_Object.cs
@@ -8,21 +8,28 @@ using UnityEngine.InputSystem;
 
 public class Spawn_Object : MonoBehaviour
 {
-  // XRRayInteractor для определения точки спавна
-  [SerializeField] private XRRayInteractor xrRayInteractor;
+	// XRRayInteractor для определения точки спавна
+	[SerializeField] private XRRayInteractor xrRayInteractor;
 
-  // Ссылка на Input Action 
-  
+	// Ссылка на Input Action 
+	
 
-  // Метод, вызываемый при нажатии на кнопку в руке
-  public void SpawnObject(GameObject _spawn)
-  {
+	// Метод, вызываемый при нажатии на кнопку в руке
+	public void SpawnObject(GameObject _spawn)
+	{
 	if (xrRayInteractor.enabled)
 	{
-	  // Создаем объект в позиции руки (с небольшим смещением для видимости)
-	  var spawnPosition = xrRayInteractor.transform.position + xrRayInteractor.transform.forward * 0.5f; 
-	  var spawnedObject = Instantiate(_spawn, spawnPosition, xrRayInteractor.transform.rotation);
+		// Создаем объект в позиции руки (с небольшим смещением для видимости)
+		var spawnPosition = xrRayInteractor.transform.position + xrRayInteractor.transform.forward * 0.5f; 
+		var spawnedObject = Instantiate(_spawn, spawnPosition, xrRayInteractor.transform.rotation);
 		spawnedObject.AddComponent<ARAnchor>(); 
 	}
-  }
+	}
+	
+	public void Show_Panel(GameObject _panel)
+	{
+		var spawnPosition = xrRayInteractor.transform.position + xrRayInteractor.transform.forward * 0.5f; 
+		_panel.transform.position = spawnPosition;
+		_panel.active = true;
+	}
 }
